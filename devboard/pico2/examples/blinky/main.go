757cd8cb0188bc3694c39a999d5dcec829022f14
Michał Derkacz <michal@Lnet.pl>
2024-12-29 01:44:13 +0100

    p: generate from svd

--- devboard/pico2/examples/blinky/main.go
+++ devboard/pico2/examples/blinky/main.go
@@ -4,7 +4,46 @@
 
 package main
 
+import (
+	"github.com/embeddedgo/pico/p/iobank"
+	"github.com/embeddedgo/pico/p/padsbank"
+	"github.com/embeddedgo/pico/p/sio"
+)
+
+const LEDpin = 25
+
 func main() {
+	ledpin := &padsbank.PADS_BANK0().GPIO[LEDpin]
+	ledio := &iobank.IO_BANK0().GPIO[LEDpin].CTRL
+	sio := sio.SIO()
+	oeset := &sio.GPIO_OE_SET
+	outset := &sio.GPIO_OUT_SET
+	outclr := &sio.GPIO_OUT_CLR
+
+	// The RP2530 pins have isolation latches that prevent changes to the pin
+	// state during boot from the deep sleep. The sequence below makes little
+	// sense for the onboard LED but demonstrates the concept of configuring
+	// the pin to known state before disabling its isolation latch.
+
+	// Setup SIO to output the low state on the LED pin..
+	outclr.Store(1 << LEDpin)
+	oeset.Store(1 << LEDpin)
+
+	// Connect the input and output of the LED pin to the SIO without disabling
+	// its isolation latch. Set the output driver strength to 4 mA.
+	ledpin.Store(padsbank.ISO | padsbank.IE | padsbank.D4MA)
+	ledio.Store(iobank.F5_SIO)
+
+	// Disable the isolation latch.
+	ledpin.ClearBits(padsbank.ISO)
+
+	// Blink
 	for {
+		for i := 0; i < 1e5; i++ {
+			outset.Store(1 << LEDpin)
+		}
+		for i := 0; i < 1e5; i++ {
+			outclr.Store(1 << LEDpin)
+		}
 	}
 }
---
+++ svd/gen.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+cd ../../../embeddedgo/pico/hal
+hal=$(pwd)
+cd ../p
+rm -rf *
+
+svdxgen github.com/embeddedgo/pico/p ../svd/*.svd
+
+for p in iobank padsbank pllsys sio xosc; do
+	cd $p
+	xgen -g *.go
+	GOOS=noos GOARCH=thumb $(emgo env GOROOT)/bin/go build -tags rp2350
+	cd ..
+done
+
+exit
+
+perlscript='
+s/package irq/$&\n\nimport "embedded\/rtos"/;
+s/ = \d/ rtos.IRQ$&/g;
+'
+
+cd $hal/irq
+rm -f *
+cp ../../p/irq/* .
+perl -pi -e "$perlscript" *.go
