10e67b4bd761f9b33c09099fd4ea5b346c1a89db
Michał Derkacz <michal@Lnet.pl>
2025-02-12 00:14:28 +0100

    hal/uart: Rx part

--- devboard/pico2/examples/uart/main.go
+++ devboard/pico2/examples/uart/main.go
@@ -17,32 +17,61 @@ import (
 
 var u *uart.Driver
 
+func puts(u *uart.Driver, s string) {
+	p := u.Periph()
+	for i := range len(s) {
+		for p.FR.LoadBits(uart.TXFF) != 0 {
+		}
+		p.DR.Store(uint32(s[i]))
+	}
+}
+
 func main() {
 	tx := pins.GP0
 	rx := pins.GP1
+	//ps := pins.PS
+
+	//ps.Setup(iomux.D4mA)
+	//ps.SetAltFunc(iomux.Null|iomux.OutHigh|iomux.OEEnable)
 
 	tx.Setup(iomux.D2mA)
-	rx.Setup(iomux.InpEn)
+	rx.Setup(iomux.InpEn | iomux.OutDis)
 	tx.SetAltFunc(iomux.UART)
 	rx.SetAltFunc(iomux.UART)
 
 	u = uart.NewDriver(uart.UART(0))
-	u.Setup(uart.Word8b, 230400)
+	u.Setup(uart.Word8b, 115200)
 	u.EnableTx()
+	u.EnableRx()
 	irq.UART0.Enable(rtos.IntPrioLow, 0)
+
 	s := "+@#$%- 0123456780 - abcdefghijklmnoprstuvwxyz - ABCDEFGHIJKLMNOPRSTUVWXYZ -%$#@+\r\n"
-	for {
-		t0 := time.Now()
-		const n = 256
-		for i := 0; i < n; i++ {
-			u.WriteString(s)
-		}
-		u.WaitTxDone()
-		dt := time.Now().Sub(t0)
-		baud := (time.Duration(n*10*len(s))*time.Second + dt/2) / dt
-		fmt.Fprintf(u, "%v, %d baud\r\n", dt, baud)
-		time.Sleep(3 * time.Second)
+	t0 := time.Now()
+	const n = 100
+	for i := 0; i < n; i++ {
+		u.WriteString(s)
 	}
+	u.WaitTxDone()
+	dt := time.Now().Sub(t0)
+	baud := (time.Duration(n*10*len(s))*time.Second + dt/2) / dt
+	fmt.Fprintf(u, "%v, %d baud\r\n", dt, baud)
+
+	var buf [128]byte
+	for range 70 {
+		u.WriteByte('>')
+	}
+	u.WriteString("\r\n")
+
+	for {
+		u.WriteString("> ")
+		n, err := u.Read(buf[:])
+		u.Write(buf[:n])
+		u.WriteString("\r\n")
+		if err != nil {
+			fmt.Fprintf(u, "error: %v\r\n", err)
+		}
+	}
+
 }
 
 //go:interrupthandler
--- hal/uart/driverrx.go
+++ hal/uart/driverrx.go
@@ -4,7 +4,11 @@
 
 package uart
 
-import "github.com/embeddedgo/pico/hal/internal"
+import (
+	"runtime"
+
+	"github.com/embeddedgo/pico/hal/internal"
+)
 
 func (d *Driver) EnableRx() {
 	internal.AtomicSet(&d.p.CR, UARTEN|RXE)
@@ -12,4 +16,59 @@ func (d *Driver) EnableRx() {
 
 func (d *Driver) DisableRx() {
 	internal.AtomicClear(&d.p.CR, RXE)
+}
+
+type Error struct {
+	s string
+}
+
+var errStr = [15]Error{
+	{"\x01fra"},
+	{"\x02par"},
+	{"\x03par+fra"},
+	{"\x04bre"},
+	{"\x05bre+fra"},
+	{"\x06bre+par"},
+	{"\x07bre+par+fra"},
+	{"\x08ove"},
+	{"\x09ove+fra"},
+	{"\x0Aove+par"},
+	{"\x0Bove+par+fra"},
+	{"\x0Cove+bre"},
+	{"\x0Dove+bre+fra"},
+	{"\x0Eove+bre+par"},
+	{"\x0Fove+bre+par+fra"},
+}
+
+// Status return the error flags as in the RSR register (see FE, PE, BE, OE).
+func (e *Error) Status() uint32 {
+	return uint32(e.s[0])
+}
+
+func (e *Error) Error() string {
+	return e.s[1:]
+}
+
+func (d *Driver) Read(s []byte) (n int, err error) {
+	if len(s) == 0 {
+		return
+	}
+	p := d.p
+	for p.FR.LoadBits(RXFE) != 0 {
+		runtime.Gosched()
+	}
+	for p.FR.LoadBits(RXFE) == 0 {
+		v := p.DR.Load()
+		s[n] = byte(v)
+		e := v >> 8 & 15
+		n++
+		if e != 0 {
+			err = &errStr[e-1]
+			break
+		}
+		if n >= len(s) {
+			break
+		}
+	}
+	return
 }
