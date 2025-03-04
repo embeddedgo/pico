8c0614b83aa3e384b9088e0e0fbfd95d405fcaf7
Michał Derkacz <michal@Lnet.pl>
2025-03-05 00:53:16 +0100

    dci/tftdci: avoid spi.SetConfig if there is no config change

--- dci/tftdci/spi.go
+++ dci/tftdci/spi.go
@@ -10,28 +10,30 @@ import (
 	"github.com/embeddedgo/pico/hal/spi"
 )
 
-// An SPI is an implementation of the display/tft.DCI interface that uses an SPI
-// peripheral to communicate with the display in what is known as 4-line mode.
+// An SPI is an implementation of the tftdrv.DCI interface that uses an SPI
+// peripheral to communicate with the display using the 4-line Serial Protocol.
 type SPI struct {
 	spi     *spi.Master
 	dc      gpio.Bit
 	csn     gpio.Bit
-	mode    spi.Config
+	mode8   spi.Config
+	mode16  spi.Config
 	rclk    int
 	wclk    int
+	is16    bool
 	started bool
-	reconf  bool
 }
 
 // NewSPI returns new SPI based implementation of tftdrv.DCI.
 func NewSPI(drv *spi.Master, csn, dc iomux.Pin, mode spi.Config, rcHz, wcHz int) *SPI {
 	dci := &SPI{
-		spi:  drv,
-		dc:   gpio.UsePin(dc),
-		csn:  gpio.UsePin(csn),
-		mode: mode,
-		rclk: rcHz,
-		wclk: wcHz,
+		spi:    drv,
+		dc:     gpio.UsePin(dc),
+		csn:    gpio.UsePin(csn),
+		mode8:  mode | spi.Word8b,
+		mode16: mode | spi.Word16b,
+		rclk:   rcHz,
+		wclk:   wcHz,
 	}
 
 	dci.csn.Set()
@@ -56,6 +58,20 @@ func start(dci *SPI) {
 	dci.csn.Clear()
 }
 
+func set8b(dci *SPI) {
+	if dci.is16 {
+		dci.is16 = false
+		dci.spi.SetConfig(dci.mode8)
+	}
+}
+
+func set16b(dci *SPI) {
+	if !dci.is16 {
+		dci.is16 = true
+		dci.spi.SetConfig(dci.mode16)
+	}
+}
+
 func (dci *SPI) Cmd(p []byte, _ int) {
 	if !dci.started {
 		start(dci)
@@ -63,7 +79,7 @@ func (dci *SPI) Cmd(p []byte, _ int) {
 	d := dci.spi
 	d.WaitTxDone()
 	dci.dc.Clear()
-	d.SetConfig(dci.mode | spi.Word8b)
+	set8b(dci)
 	d.Write(p)
 	d.WaitTxDone()
 	dci.dc.Set()
@@ -83,53 +99,48 @@ func (dci *SPI) WriteBytes(p []uint8) {
 	if !dci.started {
 		start(dci)
 	}
-	d := dci.spi
-	d.SetConfig(dci.mode | spi.Word8b)
-	d.Write(p)
+	set8b(dci)
+	dci.spi.Write(p)
 }
 
 func (dci *SPI) WriteString(s string) {
 	if !dci.started {
 		start(dci)
 	}
-	d := dci.spi
-	d.SetConfig(dci.mode | spi.Word8b)
-	d.WriteString(s)
+	set8b(dci)
+	dci.spi.WriteString(s)
 }
 
 func (dci *SPI) WriteByteN(b byte, n int) {
 	if !dci.started {
 		start(dci)
 	}
-	d := dci.spi
-	d.SetConfig(dci.mode | spi.Word8b)
-	d.WriteByteN(b, n)
+	set8b(dci)
+	dci.spi.WriteByteN(b, n)
 }
 
 func (dci *SPI) WriteWords(p []uint16) {
 	if !dci.started {
 		start(dci)
 	}
-	d := dci.spi
-	d.SetConfig(dci.mode | spi.Word16b)
-	d.Write16(p)
+	set16b(dci)
+	dci.spi.Write16(p)
 }
 
 func (dci *SPI) WriteWordN(w uint16, n int) {
 	if !dci.started {
 		start(dci)
 	}
-	d := dci.spi
-	d.SetConfig(dci.mode | spi.Word16b)
-	d.WriteWord16N(w, n)
+	set16b(dci)
+	dci.spi.WriteWord16N(w, n)
 }
 
 func (dci *SPI) ReadBytes(p []byte) {
 	if !dci.started {
 		start(dci)
 	}
+	set8b(dci)
 	d := dci.spi
-	d.SetConfig(dci.mode | spi.Word8b)
 	d.SetBaudrate(dci.rclk)
 	d.Read(p)
 	d.SetBaudrate(dci.wclk)
