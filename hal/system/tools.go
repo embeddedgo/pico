8482e8c5a3563c47993f0e449a7a7e499ccf93ec
Michał Derkacz <michal@Lnet.pl>
2025-03-06 11:49:19 +0100

    hal/system: fix bug in NextCPU

--- hal/system/tools.go
+++ hal/system/tools.go
@@ -9,11 +9,13 @@ import (
 	"sync/atomic"
 )
 
+const ncpus = 2 // NextCPU requires ncpus to be a power of 2
+
 var cpu uint32
 
 // NextCPU returns the interrupt context suitable for the rtos.IRQ.Enable
 // function. Use it to evenly (in terms of quantity, not workload) distribute
 // interrupts between available CPUs / cores.
 func NextCPU() rtos.IntCtx {
-	return rtos.IntCtx(atomic.AddUint32(&cpu, 1))
+	return rtos.IntCtx(atomic.AddUint32(&cpu, 1) & (ncpus - 1))
 }
