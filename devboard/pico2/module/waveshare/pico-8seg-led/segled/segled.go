a2061be52085806ed752da4eeb01ed77a6a2ec4a
Michał Derkacz <michal@Lnet.pl>
2025-03-07 18:51:31 +0100

    module/waveshare/pico-8seg-led: new package

---
+++ devboard/pico2/module/waveshare/pico-8seg-led/segled/segled.go
@@ -0,0 +1,12 @@
+// Copyright 2025 The Embedded Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package segled
+
+type ShiftReg struct {
+
+}
+
+
+//var Display = Disp
